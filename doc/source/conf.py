cef783a135a68623d0e7fe1ac68ead317e335183
Gudy90 <g.parimbelli90@gmail.com>
2020-04-12 01:03:36 +0200

    Use only setup.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -79,7 +79,7 @@ class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
         return MagicMock()
-MOCK_MODULES = ['scipy', 'pandas']
+MOCK_MODULES = ['numpy', 'scipy', 'pandas']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
