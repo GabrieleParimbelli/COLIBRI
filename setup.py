04524bee22382fa43febdcf54a8242bde87c2a79
Gudy90 <g.parimbelli90@gmail.com>
2020-09-22 18:28:55 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as f:
 
 setup(
     name='colibri-cosmology',
-    version='0.1.3',
+    version='0.1.4',
     author='Gabriele Parimbelli',
     author_email='g.parimbelli90@gmail.com',
     packages=find_packages(),
